74a2a715d13c787782098660516b123c8f5044a5
DS Rathore <dsrathorebsw1234@gmail.com>
2024-02-14 19:37:45 +0530

    Complete projects

--- next.config.js
+++ next.config.js
@@ -8,38 +8,7 @@ module.exports = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'images.unsplash.com',
-                pathname: "**"
-            },
-            {
-                protocol: 'https',
-                hostname: 'raw.githubusercontent.com',
-                pathname: "**"
-            },
-            {
-                protocol: 'https',
-                hostname: 'assets-global.website-files.com',
-                pathname: "**"
-            },
-            {
-                protocol: 'https',
-                hostname: 'cdn.dribbble.com',
-                pathname: "**"
-            },
-            {
-                protocol: 'https',
-                hostname: 'i0.wp.com',
-                pathname: "**"
-            },
-            {
-                protocol: 'https',
-                hostname: 'user-images.githubusercontent.com',
-                pathname: "**"
-            },
-            {
-                protocol: "https",
-                hostname: 'plus.unsplash.com',
-                pathname: "**"
+                hostname: '**'
             }
         ]
     },
